8a6e783c0d05792e8e3ed108fe4084d88f5b4a48
Ayushman <maanayush1002@gmail.com>
2023-02-16 13:29:04 +0530

    Add files via upload

---
+++ ParallelProgramming/q1.c
@@ -0,0 +1,49 @@
+#include "mpi.h"
+#include <stdio.h>
+int factorial(int num)
+{
+    if(num<2)
+    {
+        return 1;
+    }
+    return num* factorial(num-1);
+}
+
+int main(int argc, char *argv[])
+{
+    int size, rank, N;
+    int A[10], B[10], c, i;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0)
+    {
+        N = size;
+        printf("Enter %d elements of the array:\t", N);
+        for (int i = 0; i < N; i++)
+        {
+            /* code */
+            scanf("%d", &A[i]);
+        }
+    }
+
+    MPI_Scatter(A, 1, MPI_INT, &c, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("\n[Process :%d] I have received %d\n", rank, c);
+    c=factorial(c);
+
+    MPI_Gather( &c, 1, MPI_INT, B, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+      if (rank == 0)
+    {
+        N = size;
+        printf("Factorial of each element :\t");
+        for (int i = 0; i < N; i++)
+        {
+            /* code */
+            printf("%d ", B[i]);
+        }
+    }
+    MPI_Finalize();
+    return 0;
+}
