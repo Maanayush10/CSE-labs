0136d8df757dddff622aded7fee68afb177a3102
Ayushman <maanayush1002@gmail.com>
2023-02-22 12:24:17 +0530

    solved Lab4

---
+++ Internet Technologies/clasess.py
@@ -0,0 +1,17 @@
+class person:
+    "This is a person's class"
+    age= 10
+
+    def greet(self):
+        print("Hello this is Ayushman")
+
+
+# print(person.age)
+# print(person.greet)
+# print(person.__doc__)
+
+harry = person()
+print(harry.age)
+print(harry.greet())
+
+
